9da98f145790c44cb7e17060ec7bc239af16ca1c
conor shields <cshields024@gmail.com>
2024-11-01 20:23:05 -0400

    third commit

--- recursiveCountdown.js
+++ recursiveCountdown.js
@@ -13,56 +13,44 @@ function ask(questionText) {
 }
 // -------------- Your Code Below -----------------------------
 
-// Delay function for waiting between numbers
-function delay(ms) {
-  // Return a new promise that resolves after specified time
-  return new Promise((resolve) => {
-    // Use setTimeout to wait
-    setTimeout(resolve, ms)
-  })
-}
+// FUNCTION Delay function for waiting between numbers
 
-// Recursive async countdown function
-async function countdown(seconds) {
+  // Return a new promise that resolves after specified time
+  
+    // Use setTimeout to wait
+    
+
+
+
+// ASYNC FUNCTION Recursive async countdown function
+
   // Base case
-  if(seconds === 0) {
-    console.log('Blastoff!!')
-    return;
-  }
+ 
   // Print current number
-  console.log(`${seconds}...`)
+  
   // Wait for 1 second
-  setTimeout();
+ 
   // Recursive call with decremented seconds
-  await countdown(seconds - 1)
-}
-// Get input and validate it
-async function getValidInput() {
+
+
+// ASYNC FUNCTION Get input and validate it
+
   // While loop until valid input is received
-  while(true) {
+ 
     // Get input from user
-    const input = await ask('Enter a number to start countdown: ')
+    
     // Convert input to number
-    const number = parseInt(input)
+  
     // Check if input is valid
-    if(!isNaN(number) && number >= 0) {
-      return number
-    }
+   
     // Provide specific error messages
-    if(isNaN(number)) {
-      console.log('Please enter a valid number!')
-    } else if(number < 0) {
-      console.log('Please enter a positive number!')
-    }
-  }
-}
-// Main function to run the countdown program
-async function startCountdown() {
+ 
+// ASYNC FUNCTION Main function to run the countdown program
+
   // Get valid number from user
-  const number = await getValidInput();
+ 
   // Start the countdown process
-  await countdown(number)
-}
+
 // Initialize program
 console.log('Welcome to Recursive Countdown!\n')
 // Start the countdown program
